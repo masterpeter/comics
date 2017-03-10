d1b03556c60723342fc1ca0df601cfee41fa34ac
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-10 21:40:55 +0100

    WIP Edit ComicCloudRepository.java and ComicCloudRepositoryTest.java
    * ComicCloudRepository.java does the network call to MarvelApis
    * Other dependencies (public key, private key, ecc) still need to be added

--- app/src/main/java/it/mastropietro/marvelcomics/data/repository/ComicCloudRepository.java
+++ app/src/main/java/it/mastropietro/marvelcomics/data/repository/ComicCloudRepository.java
@@ -1,10 +1,19 @@
 package it.mastropietro.marvelcomics.data.repository;
 
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import it.mastropietro.marvelcomics.ComicRepository;
+import it.mastropietro.marvelcomics.data.entity.ComicEntity;
 import it.mastropietro.marvelcomics.model.Comic;
 import rx.Observable;
+import rx.Single;
+import rx.SingleSubscriber;
+import rx.functions.Func1;
 
 /**
  * Created by Angelo Mastropietro on 10/03/17.
@@ -12,7 +21,59 @@ import rx.Observable;
 
 class ComicCloudRepository implements ComicRepository {
 
-    @Override public Observable<List<Comic>> getComics() {
-        return null;
+    private final int characterId;
+    private final ComicService comicService;
+
+    @Inject
+    public ComicCloudRepository(int characterId,
+                                ComicService comicService) {
+        this.characterId = characterId;
+        this.comicService = comicService;
+    }
+
+    @Override public Single<List<Comic>> getComics() {
+        return Single.create(comicEntityList())
+                .flatMapObservable(convertToSingleItems())
+                .map(comicEntityToComic())
+                .toList()
+                .toSingle();
+    }
+
+    @NonNull private Single.OnSubscribe<List<ComicEntity>> comicEntityList() {
+        return new Single.OnSubscribe<List<ComicEntity>>() {
+            @Override
+            public void call(SingleSubscriber<? super List<ComicEntity>> singleSubscriber) {
+                try {
+                    List<ComicEntity> comicEntitiesFromNetwork = getComicEntitiesFromNetwork();
+                    singleSubscriber.onSuccess(comicEntitiesFromNetwork);
+                } catch (IOException e) {
+                    singleSubscriber.onError(e);
+                }
+            }
+        };
+    }
+
+    private List<ComicEntity> getComicEntitiesFromNetwork() throws IOException {
+        return comicService.getComicList(characterId)
+                .execute()
+                .body()
+                .getData()
+                .getComicEntities();
+    }
+
+    @NonNull private Func1<List<ComicEntity>, Observable<ComicEntity>> convertToSingleItems() {
+        return new Func1<List<ComicEntity>, Observable<ComicEntity>>() {
+            @Override public Observable<ComicEntity> call(List<ComicEntity> comicEntities) {
+                return Observable.from(comicEntities);
+            }
+        };
+    }
+
+    @NonNull private Func1<ComicEntity, Comic> comicEntityToComic() {
+        return new Func1<ComicEntity, Comic>() {
+            @Override public Comic call(ComicEntity comicEntity) {
+                return new Comic.Builder().build();
+            }
+        };
     }
 }
--- app/src/test/java/it/mastropietro/marvelcomics/data/repository/ComicCloudRepositoryTest.java
+++ app/src/test/java/it/mastropietro/marvelcomics/data/repository/ComicCloudRepositoryTest.java
@@ -1,16 +1,101 @@
 package it.mastropietro.marvelcomics.data.repository;
 
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.io.IOException;
+import java.util.List;
+
+import it.mastropietro.marvelcomics.ComicRepository;
+import it.mastropietro.marvelcomics.data.TestUtils;
+import it.mastropietro.marvelcomics.model.Comic;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Single;
+import rx.observers.TestSubscriber;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Angelo Mastropietro on 10/03/17.
  */
 public class ComicCloudRepositoryTest {
 
+    private static final int FAKE_CHARACTER_ID = 12345;
+    private static MockWebServer mockWebServer;
+    private static ComicRepository cloudRepository;
+
+    @BeforeClass
+    public static void staticSetUp() throws Exception {
+        setupMockWebServer();
+        setupComicCloudRepository();
+    }
+
+    private static void setupMockWebServer() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    private static void setupComicCloudRepository() {
+        HttpUrl baseUrl = mockWebServer.url("/");
+        ComicService comicService = getRetrofit(baseUrl).create(ComicService.class);
+        cloudRepository = new ComicCloudRepository(FAKE_CHARACTER_ID, comicService);
+    }
+
+    private static Retrofit getRetrofit(HttpUrl baseUrl) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        mockWebServer.enqueue(new MockResponse().setBody(buildComicResponse()));
+    }
+
     @Test
     public void whenGetCountryIsCalledFromCloud_returnAnObservableWithComics() throws Exception {
-        fail();
+        Single<List<Comic>> singleComicList = cloudRepository.getComics();
+
+        assertNotNull(singleComicList);
+    }
+
+    @Test
+    public void whenGetCountryIsCalledFromCloud_returnSingleWithExactlyOneList() throws Exception {
+        Single<List<Comic>> singleComicList = cloudRepository.getComics();
+
+        TestSubscriber<List<Comic>> testSubscriber = new TestSubscriber<>();
+        singleComicList.subscribe(testSubscriber);
+        List<List<Comic>> onNextEvents = testSubscriber.getOnNextEvents();
+
+        assertThat(onNextEvents.size(), is(1));
+    }
+
+    @Test
+    public void whenGetCountryIsCalledFromCloud_returnExpectedItems() throws Exception {
+        Single<List<Comic>> singleComicList = cloudRepository.getComics();
+
+        TestSubscriber<List<Comic>> testSubscriber = new TestSubscriber<>();
+        singleComicList.subscribe(testSubscriber);
+        List<Comic> comicList = testSubscriber.getOnNextEvents().get(0);
+
+        assertThat(comicList.get(0).getId(), is(62725));
+    }
+
+    private static String buildComicResponse() {
+        return TestUtils.readFile("comic_response.json");
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        mockWebServer.close();
     }
 }
