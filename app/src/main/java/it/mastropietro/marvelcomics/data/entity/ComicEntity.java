3d9935066cc3be1d12b84b83f1cb251241afe948
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-10 16:23:46 +0100

    Edit ComicEntity.java
    * Add missing getters and equals/hashcode

--- app/src/main/java/it/mastropietro/marvelcomics/data/entity/ComicEntity.java
+++ app/src/main/java/it/mastropietro/marvelcomics/data/entity/ComicEntity.java
@@ -21,4 +21,88 @@ class ComicEntity {
     @SerializedName("prices") private List<ComicEntityPrice> comicPrices;
     @SerializedName("images") private List<ComicThumbnailEntity> thumbnails;
     @SerializedName("characters") private List<CharacterSummaryEntity> characterList;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public ComicSeriesEntity getSeriesName() {
+        return seriesName;
+    }
+
+    public List<ComicEntityDate> getComicDates() {
+        return comicDates;
+    }
+
+    public List<ComicEntityPrice> getComicPrices() {
+        return comicPrices;
+    }
+
+    public List<ComicThumbnailEntity> getThumbnails() {
+        return thumbnails;
+    }
+
+    public List<CharacterSummaryEntity> getCharacterList() {
+        return characterList;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ComicEntity that = (ComicEntity) o;
+
+        if (id != that.id) return false;
+        if (pageCount != that.pageCount) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null)
+            return false;
+        if (isbn != null ? !isbn.equals(that.isbn) : that.isbn != null) return false;
+        if (format != null ? !format.equals(that.format) : that.format != null) return false;
+        if (seriesName != null ? !seriesName.equals(that.seriesName) : that.seriesName != null)
+            return false;
+        if (comicDates != null ? !comicDates.equals(that.comicDates) : that.comicDates != null)
+            return false;
+        if (comicPrices != null ? !comicPrices.equals(that.comicPrices) : that.comicPrices != null)
+            return false;
+        if (thumbnails != null ? !thumbnails.equals(that.thumbnails) : that.thumbnails != null)
+            return false;
+        return characterList != null ? characterList.equals(that.characterList) : that.characterList == null;
+
+    }
+
+    @Override public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (isbn != null ? isbn.hashCode() : 0);
+        result = 31 * result + (format != null ? format.hashCode() : 0);
+        result = 31 * result + pageCount;
+        result = 31 * result + (seriesName != null ? seriesName.hashCode() : 0);
+        result = 31 * result + (comicDates != null ? comicDates.hashCode() : 0);
+        result = 31 * result + (comicPrices != null ? comicPrices.hashCode() : 0);
+        result = 31 * result + (thumbnails != null ? thumbnails.hashCode() : 0);
+        result = 31 * result + (characterList != null ? characterList.hashCode() : 0);
+        return result;
+    }
 }
