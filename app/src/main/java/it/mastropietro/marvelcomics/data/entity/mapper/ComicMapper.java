6b724a397f7b7ba08a7cf5bb6dbdb0d857218f53
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-10 23:54:30 +0100

    Edit ComicMapper.java and related test
    * Implement ComicMapper.java logic
    * Add tests to check the happy case of a mapping

--- app/src/main/java/it/mastropietro/marvelcomics/data/entity/mapper/ComicMapper.java
+++ app/src/main/java/it/mastropietro/marvelcomics/data/entity/mapper/ComicMapper.java
@@ -1,7 +1,19 @@
 package it.mastropietro.marvelcomics.data.entity.mapper;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import it.mastropietro.marvelcomics.data.entity.CharacterEntity;
+import it.mastropietro.marvelcomics.data.entity.CharacterSummaryEntity;
 import it.mastropietro.marvelcomics.data.entity.ComicEntity;
+import it.mastropietro.marvelcomics.data.entity.ComicEntityDate;
+import it.mastropietro.marvelcomics.data.entity.ComicEntityPrice;
+import it.mastropietro.marvelcomics.data.entity.ComicImageEntity;
 import it.mastropietro.marvelcomics.model.Comic;
+import it.mastropietro.marvelcomics.model.ComicDate;
+import it.mastropietro.marvelcomics.model.ComicPrice;
 
 /**
  * Created by Angelo Mastropietro on 10/03/17.
@@ -9,7 +21,114 @@ import it.mastropietro.marvelcomics.model.Comic;
 
 public class ComicMapper implements Mapper<ComicEntity, Comic> {
 
+    // Is there something that is not optional in Marvel APIs docs? :D
+    private static final String NOT_AVAILABLE = "N/A";
+
     @Override public Comic map(ComicEntity comicEntity) {
-        return null;
+        return new Comic.Builder()
+                .id(comicEntity.getId())
+                .title(mapTitle(comicEntity))
+                .description(mapDescription(comicEntity))
+                .isbn(mapISBN(comicEntity))
+                .format(mapFormat(comicEntity))
+                .pageCount(mapPageCount(comicEntity))
+                .seriesName(mapSeriesName(comicEntity))
+                .comicDates(mapDates(comicEntity.getComicDates()))
+                .comicPrices(mapPrices(comicEntity.getComicPrices()))
+                .thumbnail(mapImage(comicEntity.getComicThumbnail()))
+                .images(mapImages(comicEntity.getComicImages()))
+                .characterList(mapCharactersSummaries(comicEntity.getCharacterSummary()))
+                .build();
+    }
+
+    private String mapPageCount(ComicEntity comicEntity) {
+        return comicEntity.getPageCount() == 0 ? NOT_AVAILABLE : String.valueOf(comicEntity.getPageCount());
+    }
+
+    private String mapTitle(ComicEntity comicEntity) {
+        return comicEntity.getTitle().isEmpty() ? NOT_AVAILABLE : comicEntity.getTitle();
+    }
+
+    private String mapDescription(ComicEntity comicEntity) {
+        return comicEntity.getDescription().isEmpty() ? NOT_AVAILABLE : comicEntity.getDescription();
+    }
+
+    private String mapISBN(ComicEntity comicEntity) {
+        return comicEntity.getIsbn().isEmpty() ? NOT_AVAILABLE : comicEntity.getIsbn();
+    }
+
+    private String mapFormat(ComicEntity comicEntity) {
+        return comicEntity.getFormat().isEmpty() ? NOT_AVAILABLE : comicEntity.getFormat();
+    }
+
+    private String mapSeriesName(ComicEntity comicEntity) {
+        return comicEntity.getSeriesName() == null ? NOT_AVAILABLE : comicEntity.getSeriesName().getName().isEmpty() ? NOT_AVAILABLE : comicEntity.getSeriesName().getName();
+    }
+
+    private List<ComicDate> mapDates(List<ComicEntityDate> comicDates) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d MMM yyyy", Locale.getDefault());
+        List<ComicDate> dates = new ArrayList<>();
+        if (comicDates != null) {
+            for (ComicEntityDate comicEntityDate : comicDates) {
+                String type = comicEntityDate.getType();
+                String date = simpleDateFormat.format(comicEntityDate.getDate());
+                if (type != null && date != null) {
+                    dates.add(new ComicDate(type, date));
+                }
+            }
+        }
+        return dates;
+    }
+
+    private List<ComicPrice> mapPrices(List<ComicEntityPrice> comicPrices) {
+        List<ComicPrice> prices = new ArrayList<>();
+        if (comicPrices != null) {
+            for (ComicEntityPrice comicPrice : comicPrices) {
+                String type = comicPrice.getType();
+                String price = comicPrice.getPrice() == 0f ? NOT_AVAILABLE : String.valueOf(comicPrice.getPrice());
+                if (type != null) {
+                    prices.add(new ComicPrice(type, price));
+                }
+            }
+        }
+        return prices;
+    }
+
+    private List<String> mapImages(List<ComicImageEntity> comicThumbnails) {
+        List<String> thumbnails = new ArrayList<>();
+        if (comicThumbnails != null) {
+            for (ComicImageEntity comicThumbnail : comicThumbnails) {
+                String image = mapImage(comicThumbnail);
+                if (!image.isEmpty()) {
+                    thumbnails.add(image);
+                }
+            }
+        }
+        return thumbnails;
+    }
+
+    private String mapImage(ComicImageEntity image) {
+        String path = image.getPath();
+        String fileExt = image.getFileExt();
+        if (path != null && fileExt != null) {
+            return path + "." + fileExt;
+        }
+        return "";
+    }
+
+    private List<String> mapCharactersSummaries(CharacterSummaryEntity summaries) {
+        List<String> characterSummaries = new ArrayList<>();
+        if (summaries != null) {
+            List<CharacterEntity> characters = summaries.getCharacters();
+            if (characters != null) {
+                for (CharacterEntity character : characters) {
+                    String name = character.getName();
+                    if (name != null) {
+                        characterSummaries.add(name);
+                    }
+                }
+            }
+        }
+        return characterSummaries;
     }
 }
--- app/src/test/java/it/mastropietro/marvelcomics/data/entity/mapper/ComicMapperTest.java
+++ app/src/test/java/it/mastropietro/marvelcomics/data/entity/mapper/ComicMapperTest.java
@@ -2,15 +2,43 @@ package it.mastropietro.marvelcomics.data.entity.mapper;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import it.mastropietro.marvelcomics.data.TestUtils;
+import it.mastropietro.marvelcomics.model.Comic;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Angelo Mastropietro on 10/03/17.
  */
 public class ComicMapperTest {
 
+    private static final ComicMapper comicMapper = new ComicMapper();
+
     @Test
-    public void name() throws Exception {
-        fail();
+    public void whenMapIsCalled_returnNotNullComic() throws Exception {
+        Comic comic = comicMapper.map(TestUtils.buildComicEntity(0));
+
+        assertNotNull(comic);
+    }
+
+    @Test
+    public void whenMapIsCalled_returnComicWithExpectedFieldsHappyPath() throws Exception {
+        Comic comic = comicMapper.map(TestUtils.buildComicEntity(0));
+
+        assertThat(comic.getId(), is(62725));
+        assertThat(comic.getTitle(), is("Secret Empire (2017)"));
+        assertThat(comic.getDescription(), containsString("The Marvel Universe"));
+        assertThat(comic.getIsbn(), is("N/A"));
+        assertThat(comic.getFormat(), is("Comic"));
+        assertThat(comic.getPageCount(), is("N/A"));
+        assertThat(comic.getSeriesName(), is("Secret Empire (2017)"));
+        assertThat(comic.getComicDates().get(0).getDate(), is("19 apr 2017"));
+        assertThat(comic.getComicPrices().get(0).getPrice(), is("N/A"));
+        assertThat(comic.getThumbnail(), containsString("58a37f7d66d13.jpg"));
+        assertThat(comic.getImages().get(0), containsString("58a37f7d66d13.jpg"));
+        assertThat(comic.getCharacters().size(), is(2));
     }
 }
