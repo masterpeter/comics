04b9206ce34bd668ba20455f46f792b319749054
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-10 18:19:10 +0100

    Add ComicCloudRepositoryTest.java

---
+++ app/src/test/java/it/mastropietro/marvelcomics/data/repository/ComicCloudRepositoryTest.java
@@ -0,0 +1,16 @@
+package it.mastropietro.marvelcomics.data.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by Angelo Mastropietro on 10/03/17.
+ */
+public class ComicCloudRepositoryTest {
+
+    @Test
+    public void whenGetCountryIsCalledFromCloud_returnAnObservableWithComics() throws Exception {
+        fail();
+    }
+}
