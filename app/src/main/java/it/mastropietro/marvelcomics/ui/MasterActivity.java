ac2794889e70ab502cf858e77fd4d134c5cf9431
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-12 13:54:06 +0100

    Add simple MasterActivity and related layout/dimen files
    * Update AndroidManifest.xml with MasterActivity as MAIN

---
+++ app/src/main/java/it/mastropietro/marvelcomics/ui/MasterActivity.java
@@ -0,0 +1,15 @@
+package it.mastropietro.marvelcomics.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.mastropietro.marvelcomics.R;
+
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+    }
+}
