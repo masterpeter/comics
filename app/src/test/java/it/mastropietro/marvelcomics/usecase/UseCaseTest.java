f076f630e3071637256101a9556c47ef96b6db30
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-12 11:20:14 +0100

    Add UseCaseTest.java

---
+++ app/src/test/java/it/mastropietro/marvelcomics/usecase/UseCaseTest.java
@@ -0,0 +1,16 @@
+package it.mastropietro.marvelcomics.usecase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Angelo Mastropietro on 12/03/17.
+ */
+public class UseCaseTest {
+
+    @Test
+    public void whenUseCaseIsExecuted_thenSubscribeToObservableReturnedByUseCase() throws Exception {
+        fail();
+    }
+}
