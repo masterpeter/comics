e683228f90b529e55487f4f007db4cb9b4dfa576
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-12 11:32:20 +0100

    Add GetComicsFromCharacterTest.java

---
+++ app/src/test/java/it/mastropietro/marvelcomics/usecase/GetComicsFromCharacterTest.java
@@ -0,0 +1,16 @@
+package it.mastropietro.marvelcomics.usecase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Angelo Mastropietro on 12/03/17.
+ */
+public class GetComicsFromCharacterTest {
+
+    @Test
+    public void whenGetComicsUseCaseIsExecuted_comicRepositoryIsCalledToRetrieveObsevable() throws Exception {
+        fail();
+    }
+}
