90b4351c9ecd43e8bdb8fa3227ec8941fa2e38d6
Angelo Mastropietro <angelo.mastropietro@immobiliare.it>
2017-03-13 11:35:04 +0100

    Edit RepositoryModule.java
    * Add @Provides for Context, CacheFactory and OkHttpClient

--- app/src/main/java/it/mastropietro/marvelcomics/data/di/RepositoryModule.java
+++ app/src/main/java/it/mastropietro/marvelcomics/data/di/RepositoryModule.java
@@ -1,16 +1,21 @@
 package it.mastropietro.marvelcomics.data.di;
 
+import android.content.Context;
+
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import it.mastropietro.marvelcomics.data.repository.Clock;
-import it.mastropietro.marvelcomics.data.repository.ClockImpl;
+import it.mastropietro.marvelcomics.data.repository.cache.CacheFactory;
+import it.mastropietro.marvelcomics.data.repository.cache.CacheFactoryImp;
+import it.mastropietro.marvelcomics.data.repository.clock.Clock;
+import it.mastropietro.marvelcomics.data.repository.clock.ClockImpl;
 import it.mastropietro.marvelcomics.data.repository.ComicCloudRepository;
 import it.mastropietro.marvelcomics.data.repository.ComicDataRepository;
 import it.mastropietro.marvelcomics.data.repository.ComicService;
 import it.mastropietro.marvelcomics.usecase.ComicRepository;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -21,8 +26,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RepositoryModule {
 
     private static final String baseUrl = "http://gateway.marvel.com/";
+
+    // These should be stored in Android KeyStore... let's keep them here for now :)
     private static final String publicKey = "6a7ed890b4b941a925202a5630d5b162";
     private static final String privateKey = "0f1d0fdf46a0bf32f962b0b9997233c0395cdf8e";
+
+    private final Context applicationContext;
+
+    public RepositoryModule(Context applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     @Singleton @Provides @Named("dataRepo")
     ComicRepository comicRepository(ComicDataRepository repository) {
@@ -55,8 +68,26 @@ public class RepositoryModule {
     }
 
     @Singleton @Provides
-    Retrofit retrofit() {
+    Context context() {
+        return applicationContext;
+    }
+
+    @Singleton @Provides
+    CacheFactory cacheFactory(CacheFactoryImp cacheFactoryImp) {
+        return cacheFactoryImp;
+    }
+
+    @Singleton @Provides
+    OkHttpClient httpClient(CacheFactory cache) {
+        return new OkHttpClient.Builder()
+                .cache(cache.createCache())
+                .build();
+    }
+
+    @Singleton @Provides
+    Retrofit retrofit(OkHttpClient httpClient) {
         return new Retrofit.Builder()
+                .client(httpClient)
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
