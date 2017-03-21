6e3e3d1c2f4477e8379743db83e469bd94113399
masterpeter <angelo.mastropietro@immobiliare.it>
2017-03-21 17:34:07 +0100

    Add ComicDiskRepository.java and related test class
    * This repository will handle caching for comics
    * Edit ComicRepository.java interface to add storeComics method
    * Edit other tests

--- app/src/main/java/it/mastropietro/marvelcomics/data/repository/ComicDataRepository.java
+++ app/src/main/java/it/mastropietro/marvelcomics/data/repository/ComicDataRepository.java
@@ -9,6 +9,7 @@ import javax.inject.Singleton;
 import it.mastropietro.marvelcomics.model.Comic;
 import it.mastropietro.marvelcomics.usecase.ComicRepository;
 import rx.Single;
+import rx.functions.Func1;
 
 /**
  * Created by Angelo Mastropietro on 10/03/17.
@@ -17,13 +18,30 @@ import rx.Single;
 public class ComicDataRepository implements ComicRepository {
 
     private final ComicRepository cloudRepo;
+    private ComicRepository diskRepo;
 
     @Inject
-    public ComicDataRepository(@Named("cloudRepo") ComicRepository cloudRepo) {
+    public ComicDataRepository(@Named("cloudRepo") ComicRepository cloudRepo,
+                               @Named("diskRepo") ComicRepository diskRepo) {
         this.cloudRepo = cloudRepo;
+        this.diskRepo = diskRepo;
     }
 
     @Override public Single<List<Comic>> getComics(int characterId, int offset) {
-        return cloudRepo.getComics(characterId, offset);
+        Single<List<Comic>> cloudComics = cloudRepo.getComics(characterId, offset);
+        Single<List<Comic>> diskComics = diskRepo.getComics(characterId, offset);
+        return diskComics.concatWith(cloudComics).takeFirst(nonEmptyList()).toSingle();
+    }
+
+    private Func1<? super List<Comic>, Boolean> nonEmptyList() {
+        return new Func1<List<Comic>, Boolean>() {
+            @Override public Boolean call(List<Comic> comics) {
+                return !comics.isEmpty();
+            }
+        };
+    }
+
+    @Override public void storeComics(List<Comic> comics, int offset) {
+        diskRepo.storeComics(comics, offset);
     }
 }
