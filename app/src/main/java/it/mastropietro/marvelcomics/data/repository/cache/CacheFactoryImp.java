4d1138b014cf944057784d4a47c3413422046a86
masterpeter <angelo.mastropietro@immobiliare.it>
2017-03-21 17:34:31 +0100

    Edit CacheFactory.java and CacheFactoryImp.java to retrieve CacheApi for shared preferences

--- app/src/main/java/it/mastropietro/marvelcomics/data/repository/cache/CacheFactoryImp.java
+++ app/src/main/java/it/mastropietro/marvelcomics/data/repository/cache/CacheFactoryImp.java
@@ -1,12 +1,16 @@
 package it.mastropietro.marvelcomics.data.repository.cache;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
 
 import java.io.File;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import it.mastropietro.marvelcomics.data.repository.clock.Clock;
 import okhttp3.Cache;
 
 /**
@@ -16,18 +20,33 @@ import okhttp3.Cache;
 public class CacheFactoryImp implements CacheFactory {
 
     private static final int FIVE_MB_CACHE = 5 * 1024 * 1024;
+    private static final String SHARED_PREF_KEY = "MarvelApp";
 
     private Context context;
+    private final Gson gson;
+    private final Clock clock;
 
     @Inject
-    public CacheFactoryImp(Context context) {
+    public CacheFactoryImp(Context context,
+                           Gson gson,
+                           Clock clock) {
         this.context = context;
+        this.gson = gson;
+        this.clock = clock;
     }
 
     @Override
     public Cache createCache() {
         File file = retrieveCacheFile();
         return new Cache(file, FIVE_MB_CACHE);
+    }
+
+    @Override public CacheApi createSharedPrefCache() {
+        return new CacheApiImp(gson, retrieveSharedPref(), clock);
+    }
+
+    private SharedPreferences retrieveSharedPref() {
+        return context.getSharedPreferences(SHARED_PREF_KEY, Context.MODE_PRIVATE);
     }
 
     private File retrieveCacheFile() {
